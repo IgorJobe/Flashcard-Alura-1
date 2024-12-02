fec12285c257b73398921fd25f84b4be86abf471
IgorJobe <00001115435735SP@al.educacao.sp.gov.br>
2024-12-02 11:15:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de Nepal?',
+    'A capital de Nepal é Katmandu'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Geografia',
+    'Quantos continentes existem no Planeta?',
+    'Ao todo existem 6 continentes sendo eles: África, América, Antártica, Ásia, Europa e Oceania. '
+)
